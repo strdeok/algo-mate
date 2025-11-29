6d05f4436bf1d3ea720480019783acf4aa1396e2
strdeok <ejraks1548@gmail.com>
2025-11-29 20:23:20 +0900

    chore: 주석 삭제

--- web/src/api/supabase.ts
+++ web/src/api/supabase.ts
@@ -3,8 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("🚨 .env 파일에 Supabase 키가 없습니다!");
-}
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
