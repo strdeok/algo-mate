30efc3abbf84c1baf4587080dcc5b799befd7cf9
strdeok <ejraks1548@gmail.com>
2025-12-29 20:03:43 +0900

    feat: 문제 없을 때 UI 추가

--- web/src/pages/overview/components/leftSection/OverviewLeftSection.tsx
+++ web/src/pages/overview/components/leftSection/OverviewLeftSection.tsx
@@ -9,6 +9,13 @@ export default function OverviewLeftSection() {
   return (
     <>
       <div className="flex flex-col gap-4">
+        {reviews?.length == 0 && (
+          <>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-text-sub">
+              오늘 복습할 문제가 없습니다. <br /> 부족한 문제들을 풀어보세요.
+            </div>
+          </>
+        )}
         {reviews?.map((review) => (
           <>
             <Card key={review.id} review={review} />
